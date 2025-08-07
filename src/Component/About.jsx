12ed56278960170621d1d45e1e0febb4f8e2be8e
techaks <aks22cse@gmail.com>
2025-08-07 10:07:11 +0530

    i

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
             responsive web pages as requirements .
           </p>
           <p className="text-2xl font-semibold pb-5">
-            <span className="text-[#43B9C0]">Full Stack Development : </span> i craft dynamic web applications using React Js and Node Js , with a strong grip on both frontend and backend technologies .
+            <span className="text-[#43B9C0]">Full Stack Development : </span> Craft dynamic web applications using React Js and Node Js , with a strong grip on both frontend and backend technologies .
           </p>
         </div>
       </div>
